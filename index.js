e1f83b1aa97b69a59d18fba210fcc579f44bffcb
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-24 16:55:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+f (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
